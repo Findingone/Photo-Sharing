fdbd4f67e5bb41e0cc3d07ed181a9e9e12bc4623
Keshav Mittal <keshav.mittal0401@gmail.com>
2023-11-08 02:58:39 +0530

    Added option to add image to the collection

--- src/components/Public/Homepage.js
+++ src/components/Public/Homepage.js
@@ -2,11 +2,32 @@ import { useEffect, useState } from "react";
 import './Homepage.css';
 import ApiCalls from "../common/Api";
 import { useSearchParams } from "react-router-dom";
+import { Dialog } from "@mui/material";
 
 const HomePage = () => {
     const [AllPhotos, setPhotos] = useState(null);
     const [params, setParams] = useSearchParams();
     const apiService = ApiCalls.getInstance();
+    const [open, setOpen] = useState(false);
+    const [selectedImage, setSelectedImage] = useState();
+    const [collectionId, setCollectionId] = useState();
+    const handleClickToOpen = (image_id) => {
+        setSelectedImage(image_id);
+        setOpen(true);
+    };
+
+    const handleToClose = () => {
+        setCollectionId("");
+        setOpen(false);
+    };
+
+    const addToCollection = () => {
+        apiService.addImageToCollection(collectionId, selectedImage).catch((err) => {
+            alert(err);
+        })
+        setOpen(false);
+    }
+
     useEffect(() => {
         if (params.get("page") == null) {
             setParams(params => {
@@ -48,6 +69,13 @@ const HomePage = () => {
         });
     }
     return <div className="master-container">
+        <Dialog open={open} onClose={handleToClose} >
+            <div className="dialog">
+                Enter the collection id to add this image: &nbsp;
+                <input value={collectionId} onChange={(event) => { setCollectionId(event.target.value) }}></input>
+                <button onClick={addToCollection}> Add to collection</button>
+            </div>
+        </Dialog>
         {AllPhotos == null ? <div>Loading</div> : <div className="total-container">
             <div>
                 {
@@ -55,6 +83,8 @@ const HomePage = () => {
                         return (<div className="image-container" key={image.id} >
                             <img src={image.urls.regular} className="cover-images" />
                             {image.liked_by_user ? <div onClick={() => { likePhoto(image.id, image.liked_by_user) }}> Unlike</div> : <div onClick={() => { likePhoto(image.id, image.liked_by_user) }}> Like</div>}
+                            <button onClick={() => { handleClickToOpen(image.id) }}>Add image to collection</button>
+                            <br />
                             <span className="text-cover" > {image.description}<br /> Likes:{image.likes} </span>
                         </div>
                         )
